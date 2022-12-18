426eebed6e3ef5b636ee86fb8749e01c71dac124
Thomas Johnston <8527295+OfficialTomJ@users.noreply.github.com>
2022-12-18 18:23:57 +1100

    Added History layout and video asset

---
+++ thomas-johnston.com/src/Layouts/MasterclassHistory.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "/node_modules/video-react/dist/video-react.css";
+import { Player } from 'video-react';
+import timelapse from '../Assets/timelapse.mp4';
+
+function MasterclassHistory() {
+    return(
+        <div className="bg-zinc-800 pt-11 pb-11">
+            <h3 className="text-white text-2xl ml-8 mr-8 pb-8"><strong>Since 2015, </strong><br></br>I have been actively investing/trading crypto and mastering the skill set.</h3>
+            <video height="200px" autoPlay loop muted>
+                <source src={timelapse} type="video/mp4"></source>
+            </video>
+            <p className="text-white ml-8 mr-8 mt-8">The journey through countless bull and bear markets hasn’t been easy, but what has stayed true is those that have conviction and stay the course see results they can only dream of.
+                <br></br><br></br>
+                Having been successful over those years, <strong>I now want to package and share everything I’ve learned so that you can take advantage of this opportunity and transform your life.</strong>
+            </p>
+        </div>
+    );
+}
+
+export default MasterclassHistory;
--- thomas-johnston.com/src/Pages/masterclass.js
+++ thomas-johnston.com/src/Pages/masterclass.js
@@ -1,12 +1,13 @@
-import MasterclassBtn from "../Components/MasterclassBtn";
 import MasterclassLanding from "../Layouts/MasterclassLanding";
 import MasterclassInterviews from "../Layouts/MasterclassInterviews"
+import MasterclassHistory from "../Layouts/MasterclassHistory"
 
 function Masterclass() {
   return (
     <>
     <MasterclassLanding></MasterclassLanding>
     <MasterclassInterviews></MasterclassInterviews>
+    <MasterclassHistory></MasterclassHistory>
     </>
   );
 }

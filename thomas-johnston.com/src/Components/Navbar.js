58b2aa2dbe5eea31187d905c98c260b51d5d5cd2
Tom <tom@MacBook-Pro-101.local>
2022-12-26 10:55:12 +1000

    Added basic sidebar menu

--- thomas-johnston.com/src/Components/Navbar.js
+++ thomas-johnston.com/src/Components/Navbar.js
@@ -1,9 +1,12 @@
 import Logo from '../Assets/logo.png'
+import Sidebar from './Sidebar';
 
 function Navbar() {
     return(
+        <>
         <img src={Logo} className="w-50"></img>
-
+        <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+        </>
     );
 }
 

3887cb2ea1f53d6fc3e42aed902539321008b1b2
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2023-07-09 18:56:04 +1000

    Removed old website

--- thomas-johnston.com/src/Components/Separator.js
+++
@@ -1,8 +0,0 @@
-
-function Separator() {
-    return(
-        <div className="bg-white opacity-10 h-px w-6/12 mx-auto mt-11 lg:mt-20"></div>
-    );
-}
-
-export default Separator;
--- thomas-johnston.com/src/Pages/PageLinks.js
+++
@@ -1,8 +0,0 @@
-
-function PageLinks() {
-    return(
-        <h1 className="">Linktree here!</h1>
-    );
-}
-
-export default PageLinks;

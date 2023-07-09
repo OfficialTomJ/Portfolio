49d5c9f1ff5ede977fac7e02fa1b8129f55ed45d
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2023-07-09 13:43:42 +1000

    Desktop Article Styles and Rich Links

--- next-portfolio/src/pages/blog/[slug].js
+++ next-portfolio/src/pages/blog/[slug].js
@@ -2,15 +2,79 @@ import React from "react";
 import fs from "fs/promises";
 import path from "path";
 import grayMatter from "gray-matter";
+import '../../app/globals.css';
+import Footer from '../../Sections/PortfolioFooter';
+import { PiArrowLeftThin } from "react-icons/pi";
 
 const BlogPost = ({ frontmatter, content }) => {
+  const paragraphs = content.split('\n\n'); // Split content into paragraphs
+
+  // Function to check if a string is a valid URL
+  const isURL = (str) => {
+    try {
+      new URL(str);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+// Function to parse links in the content
+const parseLinks = (text) => {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  const matches = text.match(urlRegex);
+
+  if (!matches) {
+    return text;
+  }
+
+  const parts = text.split(urlRegex);
+  const parsedContent = parts.map((part, index) => {
+    if (part.match(urlRegex)) {
+      return (
+        <a
+          key={index}
+          href={part}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline"
+        >
+          {part}
+        </a>
+      );
+    }
+    return part;
+  });
+
+  return parsedContent;
+};
+
+
+
+
   return (
-    <div>
-      <h1>{frontmatter.title}</h1>
-      <p>{frontmatter.date}</p>
-      <img src={frontmatter.image} alt="Blog Image" />
-      <div>{content}</div>
-    </div>
+    <>
+      <main className="bg-zinc-800 min-h-screen flex align-middle justify-center text-white pl-4 pr-4 lg:pl-0 lg:pr-0">
+        <div className="container max-w-5xl">
+          <div className="flex items-center justify-start mt-8 cursor-pointer">
+            <PiArrowLeftThin className="text-2xl mr-2 text-5xl" />
+          </div>
+          <h1 className="text-5xl mt-8 mb-2">{frontmatter.title}</h1>
+          <p className="">{frontmatter.date}</p>
+          <div className="mt-8 aspect-w-2 aspect-h-1">
+            <img src={frontmatter.image} alt="Blog Image" className="object-cover max-h-[360px] w-full rounded-2xl" />
+          </div>
+          <div className="max-w-[60%] leading-7 mt-8">
+            {paragraphs.map((paragraph, index) => (
+              <p key={index} className="mb-8">
+                {parseLinks(paragraph)}
+              </p>
+            ))}
+          </div>
+        </div>
+      </main>
+      <Footer></Footer>
+    </>
   );
 };
 
--- next-portfolio/src/posts/blvck-paris.md
+++ next-portfolio/src/posts/blvck-paris.md
@@ -1,7 +1,11 @@
 ---
-title: Blvck Paris
+title: BLVCK Paris
 date: Dec 2017 - Mar 2020
 image: /BLVCK-BG.png
 ---
 
-This is my first blog post using Markdown and Next.js.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mattis ante venenatis, tincidunt arcu nec, tempus ex. Vestibulum laoreet, risus a interdum maximus, dui ante sollicitudin eros, vitae tincidunt ligula metus sit amet purus. Quisque eget eros eget urna euismod convallis. Nulla vitae elementum mauris, id fermentum lorem. Phasellus quis tellus tortor. Aenean arcu sem, tincidunt sit amet tristique at, ullamcorper nec libero. Proin convallis metus non libero dignissim, rhoncus finibus elit tempor. Mauris egestas eros non tellus tempus rutrum at et orci.
+
+Vestibulum tempus finibus leo non commodo. Donec a molestie tortor, in posuere magna. Vivamus vel mauris porta, mollis urna eu, mattis nunc. Morbi convallis tincidunt dolor, dignissim faucibus mi tincidunt elementum. Cras volutpat lacus vel ipsum mattis, eu aliquet velit consectetur. Sed non purus a lectus consectetur venenatis. Sed ac mi arcu. Mauris placerat congue elit, a lacinia tellus pellentesque vel. Maecenas sit amet lectus urna. Praesent nunc nisi, efficitur ac pellentesque rutrum, egestas eget leo. Pellentesque rutrum vulputate viverra. Sed et dolor eget dui pulvinar rhoncus.
+
+View the Instagram here: https://instagram.com/black

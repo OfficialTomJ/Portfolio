e158d306e15ce2452bd491ed9e0808c2a8b71ef6
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2023-11-11 21:30:45 +1100

    Email Verification

--- src/Components/UserAuthModal.tsx
+++ src/Components/UserAuthModal.tsx
@@ -9,6 +9,9 @@ const UserAuthModal = () => {
   const [mode, setMode] = useState("signup");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [showVerification, setShowVerification] = useState(false);
+  const [signInError, setSignInError] = useState(null);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -16,6 +19,10 @@ const UserAuthModal = () => {
 
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+  };
+
+  const handleVerificationCodeChange = (e) => {
+    setVerificationCode(e.target.value);
   };
 
   const handleSubmit = async (e) => {
@@ -31,6 +38,9 @@ const UserAuthModal = () => {
             email: username,
           },
         });
+
+        // Switch to verification mode
+        setShowVerification(true);
         console.log("User signed up successfully");
       } else {
         // Sign in logic
@@ -38,11 +48,30 @@ const UserAuthModal = () => {
         console.log("User signed in successfully");
       }
 
-      // Clear the form after submitting (optional)
-      setUsername("");
-      setPassword("");
     } catch (error) {
       console.error("Error:", error);
+      if (mode === "signin") {
+        setSignInError("Invalid username or password. Please try again.");
+      }
+    }
+  };
+
+  const handleVerifyEmail = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Confirm the user using the verification code
+      await Auth.confirmSignUp(username, verificationCode);
+
+      // If successful, you can redirect the user or perform other actions
+      console.log("Email verification successful!");
+      // Clear the verification code after successful verification
+      setVerificationCode("");
+      // Switch back to sign-in mode after successful verification
+      setShowVerification(false);
+    } catch (error) {
+      // Handle verification error
+      console.error("Error verifying email:", error);
     }
   };
 
@@ -55,7 +84,10 @@ const UserAuthModal = () => {
               className={`cursor-pointer focus:outline-none ${
                 mode === "signup" ? "text-blue-500 font-bold" : "text-gray-500"
               }`}
-              onClick={() => setMode("signup")}
+              onClick={() => {
+                setMode("signup");
+                setShowVerification(false); // Switching back to sign-up mode
+              }}
             >
               Sign Up
             </button>
@@ -63,58 +95,97 @@ const UserAuthModal = () => {
               className={`cursor-pointer focus:outline-none ${
                 mode === "signin" ? "text-blue-500 font-bold" : "text-gray-500"
               }`}
-              onClick={() => setMode("signin")}
+              onClick={() => {
+                setMode("signin");
+                setShowVerification(false); // Switching back to sign-in mode
+              }}
             >
               Sign In
             </button>
           </div>
         </div>
-        <form>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="username"
-              type="text"
-              placeholder="Enter your email"
-              value={username}
-              onChange={handleUsernameChange}
-            />
-          </div>
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="********"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              {mode === "signup" ? "Sign Up" : "Sign In"}
-            </button>
-          </div>
-        </form>
+        {showVerification ? (
+          // Verification Code Input
+          <form onSubmit={handleVerifyEmail}>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="verificationCode"
+              >
+                Verification Code
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="verificationCode"
+                type="text"
+                placeholder="Enter verification code"
+                value={verificationCode}
+                onChange={handleVerificationCodeChange}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={handleVerifyEmail}
+              >
+                Verify Email
+              </button>
+            </div>
+          </form>
+        ) : (
+          // Sign Up / Sign In Form
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Enter your email"
+                value={username}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="********"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+            </div>
+            {mode === "signin" && (
+              <div className="mb-4 text-red-500">
+                {signInError && <p>{signInError}</p>}
+              </div>
+            )}
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                {mode === "signup" ? "Sign Up" : "Sign In"}
+              </button>
+            </div>
+          </form>
+        )}
       </div>
     </div>
   );
 };
 
 export default UserAuthModal;
+
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 import UserAuthModal from "../../Components/UserAuthModal";
 
-export default function Masterclass() {
+export default function Signup() {
   return (
     <main className="bg-zinc-800 min-h-screen flex align-middle justify-center text-white pl-4 pr-4 lg:pl-0 lg:pr-0">
         <UserAuthModal />

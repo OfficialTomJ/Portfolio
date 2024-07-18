dde12250a9462e0e04167a3cefa942cee50d5bb8
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2024-07-18 17:36:45 +1000

    Cleanup to main

--- src/pages/api/MasterclassAmplifyOne.js
+++ src/pages/api/MasterclassAmplifyOne.js
@@ -1,28 +1,10 @@
-import { Amplify } from "aws-amplify";
-import { generateClient } from "aws-amplify/api";
-import { createMasterclassOneForm } from '../../graphql/mutations';
-import Config from "../../aws-exports";
 
-Amplify.configure(Config);
-const client = generateClient();
 
 export default async function handler(req, res) {
   try {
     const { fullName, bestEmail, tradingExperience, tradingSkill, helpWithTrading, additionalComments } = req.body;
 
-    const newMasterclassOneForm = await client.graphql({
-      query: createMasterclassOneForm,
-      variables: {
-        input: {
-          "full_name": fullName,
-          "email": bestEmail,
-          "trading_experience": tradingExperience,
-          "trading_skill": tradingSkill,
-          "description": helpWithTrading,
-          "additional": additionalComments
-        }
-      }
-    });
+    console.log("Google Sheets logic here.");
 
     // Handle the response as needed
 

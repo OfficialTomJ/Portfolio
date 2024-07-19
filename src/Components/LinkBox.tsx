ddaebe963d861e43c6ae592885dc383c148ff39a
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2024-07-19 17:55:50 +1000

    Links Mobile Layout

---
+++ src/Components/LinkBox.tsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+import Link from "next/link";
+
+const LinkBox = ({ title, href }) => {
+  return (
+    <Link href={href} passHref>
+      <div className="w-full transition-all duration-300 hover:scale-[1.03]">
+        <div className="space-y-6 mb-6 w-full">
+          <div className="bg-slate-50 rounded-full pt-5 pb-5 pl-5 pr-5 w-full cursor-pointer">
+            <p className="text-black text-center font-semibold text-md">
+              {title}
+            </p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+LinkBox.propTypes = {
+  title: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
+};
+
+export default LinkBox;
--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,31 +1,52 @@
 import LinksLayout from './LinksLayout';
 import Image from "next/image";
 import Link from "next/link";
-import ProfilePic from "../../../public/profile.png";
+import ProfilePic from "../../../public/profile.jpg";
+import LinkBox from '@/Components/LinkBox';
 
 export default function Links() {
   
   return (
-    <main className="bg-zinc-800 min-h-screen flex align-middle justify-center text-white pl-4 pr-4 lg:pl-0 lg:pr-0">
-      <div className="container max-w-2xl">
-        <Image src={ProfilePic} className="w-28 pb-4" alt="ProfilePic" />
-        <p className="font-semibold">@officialtomj</p>
-        <p>
-          24 | Sydney based developer and crypto trader since 2015. <br></br>
-          <br></br>
-          Learn more about me below!
+    <main className="bg-zinc-800 min-h-screen flex flex-col align-middle justify-center text-white pl-4 pr-4 lg:pl-0 lg:pr-0 pt-24">
+      <div className="container max-w-2xl flex flex-col items-center">
+        <Image
+          src={ProfilePic}
+          className="w-28 pb-4 items-center rounded-full"
+          alt="ProfilePic"
+        />
+        <p className="font-semibold text-center">@officialtomj</p>
+        <p className="text-center pb-12">
+          🇦🇺 25 | Sydney Australia <br></br>
+          💰 Crypto since 2015
         </p>
-        <h1 className="text-4xl mt-6">My Links</h1>
-        <div className="space-y-6 flex flex-col items-center">
-          <div className="bg-slate-50 rounded-full pt-3 pb-3 w-full">
-            <p className="text-black text-center font-semibold">
-              SUBSCRIBE TO CRYPTO CHRONICLES FREE NEWSLETTER 📬
-            </p>
-          </div>
-          <div className="bg-slate-50 h-px w-1/2 opacity-30"></div>
-          
-        </div>
       </div>
+      <LinkBox
+        title="SUBSCRIBE TO CRYPTO CHRONICLES FREE NEWSLETTER 📬"
+        href="https://crypto-chronicles.beehiiv.com/"
+      />
+      <div className="bg-slate-50 h-px w-full opacity-30 mb-6"></div>
+      <LinkBox
+        title="Join the Trading Discord!"
+        href="https://discord.gg/8tK967YJ6y"
+      />
+      <LinkBox
+        title="Follow me on Instagram!"
+        href="https://www.instagram.com/officialtomj"
+      />
+      <LinkBox
+        title="Follow me on TikTok!"
+        href="https://www.tiktok.com/@officialtomj"
+      />
+
+      <LinkBox
+        title="Subscribe to me on YouTube!"
+        href="https://www.youtube.com/@OfficialTomJ"
+      />
+      <div className="bg-slate-50 h-px w-full opacity-30 mb-6"></div>
+      <LinkBox
+        title="Personal Portfolio"
+        href="https://www.thomas-johnston.com/personal"
+      />
     </main>
   );
 }

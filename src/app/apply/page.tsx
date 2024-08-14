81b03ab848baa922c7b930da8813dfb675372b29
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2024-08-15 09:28:37 +1000

    Spinner UI on Submit

--- src/app/apply/page.tsx
+++ src/app/apply/page.tsx
@@ -27,6 +27,7 @@ const Apply = () => {
   }>({});
   
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e: { target: { name: any; value: any; }; }) => {
     const { name, value } = e.target;
@@ -50,13 +51,18 @@ const Apply = () => {
 
     if (Object.keys(validationErrors).length === 0) {
       // Form is valid, proceed with submission logic
+      setIsLoading(true);
       try {
         await axios.post("/api/MasterclassAmplifyOne", formData);
         // Handle success
         setFormSubmitted(true);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
+        setIsLoading(false);
         // Handle error
+      } finally {
+        setIsLoading(false);
       }
       
     } else {
@@ -229,12 +235,37 @@ const Apply = () => {
                   <p>{errors.additionalComments}</p>
                 )}
               </div>
-              <button
-                type="submit"
-                className="bg-green-500 text-white rounded px-4 py-2 mt-4 hover:bg-green-600 focus:outline-none focus:ring focus:border-blue-300"
-              >
-                Submit
-              </button>
+              {isLoading ? (
+                <div className="flex justify-center mt-4">
+                  <svg
+                    className="animate-spin h-8 w-8 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.964 7.964 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </div>
+              ) : (
+                <button
+                  type="submit"
+                  className="bg-green-500 text-white rounded px-4 py-2 mt-4 hover:bg-green-600 focus:outline-none focus:ring focus:border-blue-300"
+                >
+                  Submit
+                </button>
+              )}
             </form>
           )}
         </div>
